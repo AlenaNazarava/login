de2ee8755f31b914988607634d446cee82289709
AlenaNazarava <gericimo87@gmail.com>
2017-12-11 22:38:55 +0100

    Update and rename signup.py to homepage.py
    login step

---
+++ pages/homepage.py
@@ -0,0 +1,38 @@
+import sys
+from base_page import BasePage
+sys.path.append("..")
+from core.ui_map import HomePageMap
+import time
+
+
+# import pdb;
+# pdb.set_trace()
+
+class Homepage(BasePage):
+
+    def __init__(self, driver):
+        super(Homepage, self).__init__(driver)
+
+    def user_is_on_homepage(self):
+        
+        self.wait_for_element_visibility(10, "id", HomePageMap['signUpFormID'])
+        self.is_element_present(5, "xpath", HomePageMap['signUpTextXPATH'])
+
+    def login(self):
+
+        self.find_element("id", HomePageMap['signUpFormID'])
+        self.find_element("xpath", HomePageMap['loginLinkXPATH'])
+        self.click(10, "xpath", HomePageMap['loginLinkXPATH'])
+        time.sleep(5)
+        self.wait_for_element_visibility(10, "xpath", HomePageMap['loginEmailXPATH'])
+        self.find_element("xpath", HomePageMap['loginEmailXPATH']).clear()
+        self.find_element("xpath", HomePageMap['loginEmailXPATH']).send_keys(HomePageMap['emailTEXT'])
+        self.find_element("xpath", HomePageMap['loginPasswordXPATH']).clear()
+        self.find_element("xpath", HomePageMap['loginPasswordXPATH']).send_keys(HomePageMap['passwordTEXT'])
+        self.click(10, "css", HomePageMap['submitButtonCSS'])
+
+    def successful_login(self):
+
+        self.check_successful_page_load()
+        self.wait_for_element_visibility(10, "xpath", HomePageMap['welcomTextXPATH'])
+
