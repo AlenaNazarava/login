55332fd0bcdf04a2811c5924a0e49a288f63a8e5
AlenaNazarava <gericimo87@gmail.com>
2017-12-11 23:25:09 +0100

    Update homepage.py
    add step for empty field

--- pages/homepage.py
+++ pages/homepage.py
@@ -17,21 +17,6 @@ class Homepage(BasePage):
         self.wait_for_element_visibility(10, "id", HomePageMap['signUpFormID'])
         self.is_element_present(5, "xpath", HomePageMap['signUpTextXPATH'])
 
-    def fill_out_signup_form(self):
-
-        self.find_element("id", HomePageMap['signUpFormID'])
-        self.is_element_present(5, "xpath", HomePageMap['signUpFirstNameXPATH'])
-        self.find_element("xpath", HomePageMap['signUpFirstNameXPATH']).clear()
-        self.find_element("xpath", HomePageMap['signUpFirstNameXPATH']).send_keys(self.random_string)
-        self.find_element("xpath", HomePageMap['signUpLastNameXPATH']).clear()
-        self.find_element("xpath", HomePageMap['signUpLastNameXPATH']).send_keys(self.random_string)
-        self.find_element("xpath", HomePageMap['signUpEmailXPATH']).clear()
-        self.find_element("xpath", HomePageMap['signUpEmailXPATH']).send_keys(self.random_email())
-        self.find_element("xpath", HomePageMap['signUpPasswordXPATH']).clear()
-        self.find_element("xpath", HomePageMap['signUpPasswordXPATH']).send_keys(self.random_string)
-        self.click(10, "id", HomePageMap['signUpCheckboxTermsConditionsID'])
-        self.click(10, "css", HomePageMap['signUpCreateAccountButtonCSS'])
-
     def login(self):
 
         self.find_element("id", HomePageMap['signUpFormID'])
@@ -67,3 +52,20 @@ class Homepage(BasePage):
 
         self.check_error_page_load()
         self.wait_for_element_visibility(10, "xpath", HomePageMap['invalidEmailPasswordXPATH'])
+
+    def login_empty_fields(self):
+
+        self.find_element("id", HomePageMap['signUpFormID'])
+        self.find_element("xpath", HomePageMap['loginLinkXPATH'])
+        self.click(10, "xpath", HomePageMap['loginLinkXPATH'])
+        time.sleep(5)
+        self.wait_for_element_visibility(10, "xpath", HomePageMap['loginEmailXPATH'])
+        self.find_element("xpath", HomePageMap['loginEmailXPATH']).clear()
+        self.find_element("xpath", HomePageMap['loginPasswordXPATH']).clear()
+        self.click(10, "css", HomePageMap['submitButtonCSS'])
+        self.check_error_page_load()
+        # different error messages?
+        self.wait_for_element_visibility(10, "xpath", HomePageMap['emptyEmailPasswordXPATH'])
+
+
+
