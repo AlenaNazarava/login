fe0bf34afa6aa9c113530740372b88d42c28cac3
AlenaNazarava <gericimo87@gmail.com>
2017-12-11 17:31:47 +0100

    Create constants.py
    browser, domain setup

---
+++ constants.py
@@ -0,0 +1,8 @@
+hh_constants = dict(Browser = "firefox", Base_URL = "https://cubits.com/")
+
+class LocatorMode:
+
+ ID           = "id"
+ NAME         = "name"
+ XPATH        = "xpath"
+ CSS_SELECTOR = "css"
