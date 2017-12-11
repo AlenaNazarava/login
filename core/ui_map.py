2b9dfbeeb14147fbe8af8f9bab5988e31d2eff3f
AlenaNazarava <gericimo87@gmail.com>
2017-12-11 23:12:08 +0100

    Update ui_map.py
    ui elements for error check

--- core/ui_map.py
+++ core/ui_map.py
@@ -14,6 +14,9 @@ HomePageMap = dict(
     loginPasswordXPATH = "//input[@id='user_password']",
     welcomTextXPATH = "//div[@class='welc__text']",
     emailTEXT = "testlogin@mailinator.com",
+    emailIncorrectTEXT = "aaa@aaa.aaa",
     passwordTEXT = "qaz123!@#",
     submitButtonCSS = "button[type='submit']",
+    invalidEmailPasswordXPATH = "//span[contains(text(),'Invalid email address or password.')]",
+    
     )
