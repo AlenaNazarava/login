b14439574e67a2bd5d74f5c22fa4257baf1abe47
AlenaNazarava <gericimo87@gmail.com>
2017-12-11 23:27:38 +0100

    Update base_page.py
    update

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import requests
+import datetime
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from constants import LocatorMode
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+from abc import abstractmethod
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from random import choice
 from string import lowercase
@@ -62,24 +65,6 @@ class BasePage(object):
             raise Exception("Unsupported locator strategy.")
         return element
 
-    def element_should_not_be_visible(self, waitTime, locatorMode, Locator):
-        element = None
-        if locatorMode == LocatorMode.ID:
-            element = WebDriverWait(self.driver, waitTime). \
-                until(EC.invisibility_of_element_located((By.ID, Locator)))
-        elif locatorMode == LocatorMode.NAME:
-            element = WebDriverWait(self.driver, waitTime). \
-                until(EC.invisibility_of_element_located((By.NAME, Locator)))
-        elif locatorMode == LocatorMode.XPATH:
-            element = WebDriverWait(self.driver, waitTime). \
-                until(EC.invisibility_of_element_located((By.XPATH, Locator)))
-        elif locatorMode == LocatorMode.CSS_SELECTOR:
-            element = WebDriverWait(self.driver, waitTime). \
-                until(EC.invisibility_of_element_located((By.CSS_SELECTOR, Locator)))
-        else:
-            raise Exception("Unsupported locator strategy.")
-        return element
-
     def wait_until_element_clickable(self, waitTime, locatorMode, Locator):
         element = None
         if locatorMode == LocatorMode.ID:
@@ -98,15 +83,6 @@ class BasePage(object):
             raise Exception("Unsupported locator strategy.")
         return element
 
-    def page_url(self):
-        return self.driver.current_url
-
-    def close_window(self):
-        self.driver.close()
-
-    def go_to_page(self, url):
-        self.driver.get(url)
-
     def find_element(self, locatorMode, Locator):
         element = None
         if locatorMode == LocatorMode.ID:
@@ -121,20 +97,6 @@ class BasePage(object):
             raise Exception("Unsupported locator strategy.")
         return element
 
-    def find_elements(self, locatorMode, Locator):
-        element = None
-        if locatorMode == LocatorMode.ID:
-            element = self.driver.find_elements_by_id(Locator)
-        elif locatorMode == LocatorMode.NAME:
-            element = self.driver.find_elements_by_name(Locator)
-        elif locatorMode == LocatorMode.XPATH:
-            element = self.driver.find_elements_by_xpath(Locator)
-        elif locatorMode == LocatorMode.CSS_SELECTOR:
-            element = self.driver.find_elements_by_css_selector(Locator)
-        else:
-            raise Exception("Unsupported locator strategy.")
-        return element
-
     def fill_out_field(self, locatorMode, Locator, text):
         self.find_element(locatorMode, Locator).clear()
         self.find_element(locatorMode, Locator).send_keys(text)
@@ -142,24 +104,17 @@ class BasePage(object):
     def click(self, waitTime, locatorMode, Locator):
         self.wait_until_element_clickable(waitTime, locatorMode, Locator).click()
 
-    def get_element_text(self, locatorMode, Locator):
-        self.find_element(locatorMode, Locator).text
-
     def check_successful_page_load(self):
         requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
         current_url = self.driver.current_url
         r = requests.get(current_url, verify=False)
+        print r.status_code
         assert (r.status_code == 200)
 
-    def random_string(self):
-        string = "".join(choice(lowercase) for i in range(5)) + 'smoke'
-        return string
+    def check_error_page_load(self):
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        current_url = self.driver.current_url
+        r = requests.get(current_url, verify=False)
+        assert (r.status_code == 503)
 
-    def random_phone_number(self):
-        number = "077000" + ''.join(choice(digits) for _ in range(5))
-        return number
-
-    def random_email(self):
-        email = "".join(choice(lowercase) for i in range(10)) + '@mailinator.com'
-        return email
 
