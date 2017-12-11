0f7ddee1cf857c69413a04bee7603243f383a6a5
AlenaNazarava <gericimo87@gmail.com>
2017-12-11 17:22:52 +0100

    Create ui_map.py
    locators of ui elemants

---
+++ core/ui_map.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+HomePageMap = dict(
+    signUpFormID = "initial_signup_form",
+    signUpFirstNameXPATH = "//input[@id='user_first_name']",
+    signUpLastNameXPATH = "//input[@id='user_last_name']",
+    signUpEmailXPATH = "//input[@id='user_email']",
+    signUpPasswordXPATH = "//input[@id='user_password']",
+    signUpCheckboxTermsConditionsID = "user_terms_of_service",
+    signUpCreateAccountButtonCSS = "button[type='submit']",
+    signUpTextXPATH = "//ul[@class='list-circle']//li[contains(text(), 'Buy and Sell Bitcoin instantly at any time')]",
+    )
