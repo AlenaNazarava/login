6d550ce9314cc264af3e4cb6e42e4eb6d4392d9f
AlenaNazarava <gericimo87@gmail.com>
2017-12-11 23:26:20 +0100

    Update ui_map.py
    error message for empty

--- core/ui_map.py
+++ core/ui_map.py
@@ -1,14 +1,6 @@
 # coding: utf-8
 
 HomePageMap = dict(
-    signUpFormID = "initial_signup_form",
-    signUpFirstNameXPATH = "//input[@id='user_first_name']",
-    signUpLastNameXPATH = "//input[@id='user_last_name']",
-    signUpEmailXPATH = "//input[@id='user_email']",
-    signUpPasswordXPATH = "//input[@id='user_password']",
-    signUpCheckboxTermsConditionsID = "user_terms_of_service",
-    signUpCreateAccountButtonCSS = "button[type='submit']",
-    signUpTextXPATH = "//ul[@class='list-circle']//li[contains(text(), 'Buy and Sell Bitcoin instantly at any time')]",
     loginLinkXPATH = "//a[@href='/users/sign_in']",
     loginEmailXPATH = "//input[@id='user_email']",
     loginPasswordXPATH = "//input[@id='user_password']",
@@ -18,5 +10,5 @@ HomePageMap = dict(
     passwordTEXT = "qaz123!@#",
     submitButtonCSS = "button[type='submit']",
     invalidEmailPasswordXPATH = "//span[contains(text(),'Invalid email address or password.')]",
-    
+    emptyEmailPasswordXPATH = "//span[contains(text(),'Invalid email or password.')]",
     )
