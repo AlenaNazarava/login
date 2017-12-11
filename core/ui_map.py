416a92753eaa527bde16ae94dd6bfcd7a3b7a6ac
AlenaNazarava <gericimo87@gmail.com>
2017-12-11 22:39:47 +0100

    Update ui_map.py
    add login elements

--- core/ui_map.py
+++ core/ui_map.py
@@ -9,4 +9,11 @@ HomePageMap = dict(
     signUpCheckboxTermsConditionsID = "user_terms_of_service",
     signUpCreateAccountButtonCSS = "button[type='submit']",
     signUpTextXPATH = "//ul[@class='list-circle']//li[contains(text(), 'Buy and Sell Bitcoin instantly at any time')]",
+    loginLinkXPATH = "//a[@href='/users/sign_in']",
+    loginEmailXPATH = "//input[@id='user_email']",
+    loginPasswordXPATH = "//input[@id='user_password']",
+    welcomTextXPATH = "//div[@class='welc__text']",
+    emailTEXT = "testlogin@mailinator.com",
+    passwordTEXT = "qaz123!@#",
+    submitButtonCSS = "button[type='submit']",
     )
