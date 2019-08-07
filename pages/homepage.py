c99f04eb28c63c2e4242e462634567f7ef8dbaec
alena0604 <gericimo87@gmail.com>
2019-08-07 14:42:18 +0200

    Update homepage.py

--- pages/homepage.py
+++ pages/homepage.py
@@ -5,11 +5,7 @@ from core.ui_map import HomePageMap
 import time
 
 
-# import pdb;
-# pdb.set_trace()
-
 class Homepage(BasePage):
-
     def __init__(self, driver):
         super(Homepage, self).__init__(driver)
 
@@ -18,7 +14,6 @@ class Homepage(BasePage):
         self.is_element_present(5, "xpath", HomePageMap['signUpTextXPATH'])
 
     def login(self):
-
         self.find_element("id", HomePageMap['signUpFormID'])
         self.find_element("xpath", HomePageMap['loginLinkXPATH'])
         self.click(10, "xpath", HomePageMap['loginLinkXPATH'])
@@ -31,12 +26,10 @@ class Homepage(BasePage):
         self.click(10, "css", HomePageMap['submitButtonCSS'])
 
     def successful_login(self):
-
         self.check_successful_page_load()
         self.wait_for_element_visibility(10, "xpath", HomePageMap['welcomTextXPATH'])
 
     def login_invalid_email(self):
-
         self.find_element("id", HomePageMap['signUpFormID'])
         self.find_element("xpath", HomePageMap['loginLinkXPATH'])
         self.click(10, "xpath", HomePageMap['loginLinkXPATH'])
@@ -48,13 +41,11 @@ class Homepage(BasePage):
         self.find_element("xpath", HomePageMap['loginPasswordXPATH']).send_keys(HomePageMap['passwordTEXT'])
         self.click(10, "css", HomePageMap['submitButtonCSS'])
 
-        def error_login(self):
-
+    def error_login(self):
         self.check_successful_page_load()
         self.wait_for_element_visibility(10, "xpath", HomePageMap['invalidEmailPasswordXPATH'])
 
     def login_empty_fields(self):
-
         self.find_element("id", HomePageMap['signUpFormID'])
         self.find_element("xpath", HomePageMap['loginLinkXPATH'])
         self.click(10, "xpath", HomePageMap['loginLinkXPATH'])
@@ -64,7 +55,6 @@ class Homepage(BasePage):
         self.find_element("xpath", HomePageMap['loginPasswordXPATH']).clear()
         self.click(10, "css", HomePageMap['submitButtonCSS'])
         self.check_successful_page_load()
-        # different error messages?
         self.wait_for_element_visibility(10, "xpath", HomePageMap['emptyEmailPasswordXPATH'])
 
 
